eb2896ff12d2e986adcf2a3d24f31a8e6a1cdd24
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-29 17:27:22 +0800

    Add bottom border to question input field

--- zuckbot/gamestates/objects/question_input.py
+++ zuckbot/gamestates/objects/question_input.py
@@ -1,4 +1,7 @@
+import pygame
+
 from .pygame_textinput import TextInput
+
 
 class Question_Input:
     """Input field where user enters what they'd like to ask Zuckbot."""
@@ -9,12 +12,14 @@ class Question_Input:
         self.settings = gamestate.settings
         self.screen = gamestate.screen
         self.text_input = self._get_text_input()
+        self.bottom_border_rect = pygame.Rect(50, 105, 1100, 1)
 
 
     def _get_text_input(self):
-        """Returns a pygame_textinput.TextInput object acting a text field of 
+        """Returns a pygame_textinput.TextInput object acting as text field of 
         the object."""
         return TextInput(
+            initial_string="> ",
             font_family=self.settings.font_regular_filename,
             antialias=True,
             text_color=self.settings.font_color,
@@ -33,4 +38,9 @@ class Question_Input:
         self.screen.blit(
             self.text_input.get_surface(),
             self.settings.question_input_dest,
+        )
+        pygame.draw.rect(
+            self.screen, 
+            self.settings.font_color, 
+            self.bottom_border_rect
         )
