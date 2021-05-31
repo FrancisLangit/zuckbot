712691ec8999f5b513b45964eaa604b3ac4fd9ba
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-31 16:15:24 +0800

    Add zuckbot image files to data folder

--- zuckbot/settings.py
+++ zuckbot/settings.py
@@ -1,7 +1,8 @@
 import pygame
 
+
 class Settings:
-    """"""
+    """Settings of the application."""
 
     def __init__(self):
         """Initialize Settings class attributes."""
@@ -13,10 +14,20 @@ class Settings:
 
         # Font
         self.font_color = (255, 255, 255)
-        self.font_bold_filename = './zuckbot/data/Open Sans Bold.ttf'
-        self.font_light_filename = './zuckbot/data/Open Sans Light.ttf'
-        self.font_regular_filename = './zuckbot/data/Open Sans Regular.ttf'
+        self.font_bold_filename = './zuckbot/data/fonts/open_sans_bold.ttf'
+        self.font_light_filename = './zuckbot/data/fonts/open_sans_light.ttf'
+        self.font_regular_filename = './zuckbot/data/fonts/open_sans_reg.ttf'
 
         # Question Input
         self.question_input_max_length = 64
         self.question_input_dest = (50, 50)
+
+        # Zuckbot
+        self.zuckbot_neutral_filename = (
+            './zuckbot/data/images/zuckbot_neutral.jpg')
+        self.zuckbot_affirmative_filename = (
+            './zuckbot/data/images/zuckbot_affirmative.jpg')
+        self.zuckbot_noncomittal_filename = (
+            './zuckbot/data/images/zuckbot_noncomittal.jpg')
+        self.zuckbot_negative_filename = (
+            './zuckbot/data/images/zuckbot_negative.jpg')
